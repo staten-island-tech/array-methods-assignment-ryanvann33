43f6b97140e6a3f9ff0030a5c789dbaed32ba876
ryanvann33 <rvann377@gmail.com>
2022-11-01 13:57:43 -0400

    peeeeeeeeeee

---
+++ index.js
@@ -0,0 +1,24 @@
+const arr = [
+  { name: "Genji", type: "DPS", weapon: "Shuriken", ultimate: "Dragonblade" },
+
+  {
+    name: "Soldier76",
+    type: "DPS",
+    weapon: "Heavy",
+    ultimate: "Tactical Visor",
+  },
+
+  {
+    name: "Sigma",
+    type: "Tank",
+    weapon: "Hyperspheres",
+    ultimate: "Gravitic Flux",
+  },
+];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+const arr2 = [true, false, "yay!", [3, 6, 7], null, 9];
+console.log(arr2);
