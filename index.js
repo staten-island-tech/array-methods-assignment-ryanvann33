343352b82d1e4d3cdf59396b86d5ef1c8e66872b
ryanvann33 <rvann377@gmail.com>
2022-11-02 13:59:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const arr = [
-  { name: "Genji", type: "DPS", weapon: "Shuriken", ultimate: "Dragonblade" },
+  {
+    name: "Genji",
+    type: "DPS",
+    weapon: "Shuriken",
+    ultimate: "Dragonblade",
+    numbers: [1, 2, 3, 4],
+  },
 
   {
     name: "Soldier76",
     type: "DPS",
     weapon: "Heavy",
     ultimate: "Tactical Visor",
+    numbers: [5, 6, 7, 8],
   },
 
   {
     name: "Sigma",
     type: "Tank",
+    numbers: [9, 10, 11, 12],
     weapon: "Hyperspheres",
     ultimate: "Gravitic Flux",
   },
@@ -18,7 +26,6 @@ const arr = [
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
-console.log(arr[3]);
+console.log(arr[2]);
 
-const arr2 = [true, false, "yay!", [3, 6, 7], null, 9];
-console.log(arr2);
+arr.forEach((arr) => console.log(arr.numbers));
